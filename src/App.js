56898e7aaf6ee89c47adb5314a2ab1db2cb0ade6
obinna <obinnae23@gmail.com>
2022-04-13 14:12:36 +0200

    front-end netflix clone

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOrginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending NOW" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
